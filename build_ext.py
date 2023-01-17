4c3f297598a01373e5bb472f6ced043ca4e5a997
Danilo Horta <danilo.horta@pm.me>
2023-01-17 13:15:15 +0000

    building

--- build_ext.py
+++ build_ext.py
@@ -3,16 +3,34 @@ import shutil
 import subprocess
 import tarfile
 import urllib.request
+from dataclasses import dataclass
 from pathlib import Path
 
 PWD = Path(os.path.dirname(os.path.abspath(__file__)))
-USER = "horta"
-PROJECT = "deciphon"
-VERSION = "0.5.0"
+DST = PWD / ".ext_deps"
+
 CMAKE_OPTS = [
+    "-DCMAKE_PREFIX_PATH=",
     "-DCMAKE_BUILD_TYPE=Release",
-    "-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
     "-DBUILD_SHARED_LIBS=ON",
+]
+CPM_OPTS = ["-DCPM_USE_LOCAL_PACKAGES=ON"]
+
+
+@dataclass
+class Dependency:
+    user: str
+    project: str
+    version: str
+    cmake_opts: list[str]
+
+
+DEPS = [
+    Dependency("horta", "logaddexp", "2.1.14", CMAKE_OPTS),
+    Dependency("horta", "elapsed", "3.1.2", CMAKE_OPTS),
+    Dependency("EBI-Metagenomics", "lip", "0.5.0", CMAKE_OPTS),
+    Dependency("EBI-Metagenomics", "imm", "2.1.10", CMAKE_OPTS + CPM_OPTS),
+    Dependency("EBI-Metagenomics", "deciphon", "0.3.3", CMAKE_OPTS + CPM_OPTS),
 ]
 
 
@@ -29,26 +47,29 @@ def get_cmake_bin():
 
 
 def cleanup_intree_artifacts():
-    rm(PWD / "deciphon", "cffi.c")
+    rm(PWD / "deciphon", "cffi.*")
     rm(PWD / "deciphon", "*.o")
     rm(PWD / "deciphon", "*.so")
     rm(PWD / "deciphon", "*.dylib")
 
 
-def build_deps():
-    ext_dir = PWD / ".ext_deps"
-    shutil.rmtree(ext_dir, ignore_errors=True)
+def cleanup_ext_deps():
+    shutil.rmtree(DST, ignore_errors=True)
 
-    prj_dir = ext_dir / f"{PROJECT}-{VERSION}"
+
+def build_dep(dep: Dependency):
+    ext_dir = DST
+
+    prj_dir = ext_dir / f"{dep.project}-{dep.version}"
     build_dir = prj_dir / "build"
     os.makedirs(build_dir, exist_ok=True)
 
-    url = f"https://github.com/{USER}/{PROJECT}/archive/refs/tags/v{VERSION}.tar.gz"
+    url = f"https://github.com/{dep.user}/{dep.project}/archive/refs/tags/v{dep.version}.tar.gz"
 
     with urllib.request.urlopen(url) as rf:
         data = rf.read()
 
-    tar_filename = f"{PROJECT}-{VERSION}.tar.gz"
+    tar_filename = f"{dep.project}-{dep.version}.tar.gz"
 
     with open(ext_dir / tar_filename, "wb") as lf:
         lf.write(data)
@@ -58,16 +79,12 @@ def build_deps():
 
     cmake_bin = get_cmake_bin()
     subprocess.check_call(
-        [cmake_bin, "-S", str(prj_dir), "-B", str(build_dir)] + CMAKE_OPTS
+        [cmake_bin, "-S", str(prj_dir), "-B", str(build_dir)] + dep.cmake_opts
     )
     subprocess.check_call([cmake_bin, "--build", str(build_dir), "--config", "Release"])
     subprocess.check_call(
         [cmake_bin, "--install", str(build_dir), "--prefix", str(ext_dir)]
     )
-    rm(ext_dir / "lib", "*.dylib")
-    rm(ext_dir / "lib", "*.so*")
-    rm(ext_dir / "lib64", "*.dylib")
-    rm(ext_dir / "lib64", "*.so*")
 
 
 if __name__ == "__main__":
@@ -76,23 +93,13 @@ if __name__ == "__main__":
     ffibuilder = FFI()
 
     cleanup_intree_artifacts()
-    build_deps()
-    # library_dirs = [PWD / ".ext_deps" / "lib", PWD / ".ext_deps" / "lib64"]
-    library_dirs = [
-        PWD / ".ext_deps" / f"deciphon-{VERSION}/build",
-        PWD / ".ext_deps" / f"deciphon-{VERSION}/build/press",
-        PWD / ".ext_deps" / "lib64",
-    ]
-    # include_dirs = [PWD / ".ext_deps" / "include"]
-    # include_dirs = [PWD / ".ext_deps"]
-    include_dirs = [
-        PWD / ".ext_deps" / f"deciphon-{VERSION}",
-        PWD / ".ext_deps" / f"deciphon-{VERSION}/build/_deps/lite-pack-src/include",
-        PWD / ".ext_deps" / f"deciphon-{VERSION}/build/_deps/imm-src/include",
-        PWD / ".ext_deps" / f"deciphon-{VERSION}/build/_deps/hmmer-reader-src/include",
-        PWD / ".ext_deps" / f"deciphon-{VERSION}/build/_deps/elapsed-src/include",
-        PWD / ".ext_deps" / f"deciphon-{VERSION}/build/_deps/logaddexp-src/include",
-    ]
+    cleanup_ext_deps()
+
+    for dep in DEPS:
+        build_dep(dep)
+
+    library_dirs = [DST / "lib", DST / "lib64"]
+    include_dirs = [DST / "include"]
 
     with open(PWD / "deciphon" / "interface.h", "r") as f:
         interface_h = f.read()
@@ -101,7 +108,7 @@ if __name__ == "__main__":
     ffibuilder.set_source(
         "deciphon.cffi",
         """
-        #include "press/press.h"
+        #include "deciphon/press.h"
         """,
         language="c",
         libraries=["deciphon"],
